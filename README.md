31cec2d20b80fbe147756d4ce3f07db0706059b4
tobenna <eliyahscopy@gmail.com>
2022-11-22 21:03:44 +0100

    almost final touch

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # NFT-collection
+
 still A tutorial
-but I learnt how to add time to presales and also how to have public and private sales 
+but I learnt how to add time to presales and also how to have public and private sales
 also how to start a minting process
+learnt how to include meta data
--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,5 +3,13 @@
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
 
-  const name = ``;
+  const name = `Crypto Dev # ${tokenId}`;
+  const description = "CryptoDev is an NFT collection for web 3 developers";
+  const image = `https://raw.githubusercontent.com/tobezhanabi/Nft-collection/main/my-app/public/cryptodevs/${tokenId}.svg`;
+
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
+  });
 }
